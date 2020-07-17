dcf2cf8d32779ce4f13cbb25a1f47c1e509190e7
abdullah <hbkamk@gmail.com>
2020-07-17 18:26:55 +0600

    Updates

---
+++ precache-manifest.7759d114e9536c56a89e0c53ab1f3530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfbbd708d5b93dd6e4987d071ced4aa",
+    "url": "/Budget-App/index.html"
+  },
+  {
+    "revision": "171b93116b0dbe7e2081",
+    "url": "/Budget-App/static/css/main.90884b94.chunk.css"
+  },
+  {
+    "revision": "d33c9c40ac2d13fc8f74",
+    "url": "/Budget-App/static/js/2.3dcc6d43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Budget-App/static/js/2.3dcc6d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171b93116b0dbe7e2081",
+    "url": "/Budget-App/static/js/main.42948f0b.chunk.js"
+  },
+  {
+    "revision": "892bf659fc2c43f8f8ea",
+    "url": "/Budget-App/static/js/runtime-main.7acf3173.js"
+  }
+]);
